0c3f1c37c0faf5e304c57caeb3329c5ee0023fe8
Victor Omah <14thchief@gmail.com>
2022-09-14 19:33:47 -0700

    Quick fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-*main prints '_putchar'
-*/
- 
-int main(){
-	_putchar()
-	return 0;
-}
+  * main - Entry point
+  *
+  * Decsription: prints _putchar and a new line.
+  *
+  * Return:Always 0.
+  */
+ int main(void)
+ {
+         char *c = "_putchar";
+
+         while (*c)
+         {
+                 _putchar(*c);
+                 c++;
+         }
+         _putchar('\n');
+ return (0);
+ }
