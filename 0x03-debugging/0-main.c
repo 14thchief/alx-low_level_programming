fedc940285b43198ef28e86789921f33148b4859
Victor Omah <14thchief@gmail.com>
2022-09-15 15:05:27 -0700

    Sharp one

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - test imported function
- * Return: 0 Always
+ * main - test function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int 1 = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
