b6879e615dc2053ae7d2561e3c226d1f59156165
Victor Omah <14thchief@gmail.com>
2022-09-14 21:05:01 -0700

    Quick commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 /**
  * _islower - entry point
  * Return: 1 if lowercase and 0 if not
- * @chr - ASCII of character
+ * @chr: ASCII of character
  */
 
 int _islower(int chr)
 {
 	int r = (chr >= 97 && chr <= 122);
-	
+
 	return (r);
 }
