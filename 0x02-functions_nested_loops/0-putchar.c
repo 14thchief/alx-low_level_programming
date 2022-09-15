d7ba23109a86ba19787fbe41fb24b4bdeda33258
Victor Omah <14thchief@gmail.com>
2022-09-14 19:39:40 -0700

    Quick fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,13 +10,11 @@
   */
  int main(void)
  {
-         char *c = "_putchar";
-
-         while (*c)
-         {
-                 _putchar(*c);
-                 c++;
-         }
-         _putchar('\n');
- return (0);
+	char *c ="_putchar";
+ 	while(*c)
+	 _putchar(*c)
+	 	c++;
+ 	}
+ 	_putchar('\n');
+	return (0);
  }
