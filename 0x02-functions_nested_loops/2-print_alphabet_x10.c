cbb8be073eec7122a4d3426cea06fe187bbc1ae4
Victor Omah <14thchief@gmail.com>
2022-09-14 20:36:45 -0700

    Quick commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char chr = 'a';
-	char end = 'z';
 	int i = 0;
 
 	while (i < 10)
 	{
+		char chr = 'a';
+		char end = 'z';
+
 		while (chr <= end)
 		{
 			_putchar(chr);
