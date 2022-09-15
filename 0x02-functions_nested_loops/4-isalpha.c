5bd171111d9c39d86d6df4180073790a8ec154b1
Victor Omah <14thchief@gmail.com>
2022-09-14 21:13:59 -0700

    Quick commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet
+ * Return: 1 or 0
+ * @chr: charecter to check
+ */
+
+int _isalpha(int chr)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
