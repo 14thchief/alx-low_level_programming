d9feeb28ea136d2017aeed10759c8aa132e72db8
Victor Omah <14thchief@gmail.com>
2022-09-19 21:37:01 -0700

    save

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_array - function to print
+ * @a: array
+ * @n: specified number
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf('\n');
+}
