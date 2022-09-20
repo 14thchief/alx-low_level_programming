3791d863f44ad7384e420af691f11828c3b3f221
Victor Omah <14thchief@gmail.com>
2022-09-19 19:35:23 -0700

    quick one

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses string
+ * @s: string to reverse
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char str;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		str += s[i];
+		i--;
+	}
+	str += '\0';
+
+	return (str);
+}
