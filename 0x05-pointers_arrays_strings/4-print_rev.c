2b0dfad43e0892c095bce4842f854cc23383e5cf
Victor Omah <14thchief@gmail.com>
2022-09-19 19:24:18 -0700

    quick one

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * print_rev - prints string in reverse
+ * @s: string to print
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
