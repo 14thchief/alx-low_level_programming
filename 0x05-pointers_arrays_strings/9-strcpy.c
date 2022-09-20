91e210c38fbeff9869f79e2d7c297ee7e76fb9e5
Victor Omah <14thchief@gmail.com>
2022-09-19 21:46:20 -0700

    get right

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _strcpy - function to copy pointed string
+ * @dest: location address
+ * @char: string to copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
