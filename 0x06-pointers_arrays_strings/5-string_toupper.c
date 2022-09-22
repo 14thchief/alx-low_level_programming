5e0b8e881dd17e965abe06306748e00ef969df1f
Victor Omah <14thchief@gmail.com>
2022-09-22 13:52:42 -0700

    quick save

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - converts string to uppercase
+ * @s: string
+ * Return: capitalized string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
