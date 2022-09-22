5a5f00cfe549e9e1e3f7f5a5903fbfb68ab4b372
Victor Omah <14thchief@gmail.com>
2022-09-22 14:29:30 -0700

    quick save

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+/**
+ * cap_string - capitalize every string
+ * @s: string
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	char delimeters[] = "\t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] -= 32;
+			for (j = 0; delimeters[j] != '\0'; j++)
+			{
+				if (s[i] == delimeter[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
