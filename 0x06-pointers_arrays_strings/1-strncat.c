5fc4680f4888da94ded8c6d0fd2c1100a494543a
Victor Omah <14thchief@gmail.com>
2022-09-21 22:48:03 -0700

    save

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+/**
+ * _strncat - concatenates two strings with delimiter
+ * @dest: first string
+ * @src: second string
+ * @n: bytes delimiter for second string
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int destLength = 0;
+
+	while (dest[destLength] != '\0')
+	{
+		destLength++;
+	}
+	for (i = 0; src[i] != '\0' && i <= n; i++)
+	{
+		dest[destLength + i] = src[i];
+	}
+
+	return (dest);
+}
