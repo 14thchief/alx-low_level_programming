ff405d2cb2437b36146a2b737024cbe70ff80417
Victor Omah <14thchief@gmail.com>
2022-09-22 18:12:30 -0700

    quick save

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,42 +1,46 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of bauer's day
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
  * Return: void
  */
-
 void jack_bauer(void)
 {
-	int hour1 = 0;
-	int hour2 = 0;
-	int min1 = 0;
-	int min2 = 0;
-	int doubleDig = 10;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	while (hour2 < 2)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		if (hour2 == 2)
+		if (hours_tens == '2')
 		{
-			doubleDig = 3;
+			hours_max = '4';
 		}
-		while (hour1 < doubleDig)
+		hours_ones = '0';
+		while (hours_ones < hours_max)
 		{
-			while (min2 < 6)
+			minutes_tens = '0';
+			while (minutes_tens < '6')
 			{
-				while (min1 < 10)
+				minutes_ones = '0';
+				while (minutes_ones < 58)
 				{
-					_putchar('0' + hour2);
-					_putchar('0' + hour1);
+					_putchar(hours_tens);
+					_putchar(hours_ones);
 					_putchar(':');
-					_putchar('0' + min2);
-					_putchar('0' + min1);
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
 					_putchar('\n');
-					min1++;
+					minutes_ones++;
 				}
-				min2++;
+				minutes_ones = '0';
+				minutes_tens++;
 			}
-			hour1++;
+			minutes_tens = '0';
+			hours_ones++;
 		}
-		hour2++;
+		hours_ones = '0';
+		hours_tens++;
 	}
 }
