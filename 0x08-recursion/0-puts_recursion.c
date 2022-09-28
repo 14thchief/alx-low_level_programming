7b67b75620796f2ce5e55646a3f9a55e8d4054d0
Victor Omah <14thchief@gmail.com>
2022-09-28 15:01:17 -0700

    quick save

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _puts_recursion - recursively p[rints a char to standard output
+ * @s: string to print
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int n = 0;
+
+	if (s[n] == '\0')
+	{
+		return;
+	}
+
+	_putchar(s[n]);
+	n ++;
+	_puts_recursion(s);
+}
