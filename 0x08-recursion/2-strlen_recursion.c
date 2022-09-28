10b07ed7808325fe5ce3b3c08fdbf43ef3abab18
Victor Omah <14thchief@gmail.com>
2022-09-28 15:27:44 -0700

    quick save

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen_reursion - gets length of string
+ * @s: string
+ * return: integer length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+	{
+		n += _strlen_recursion(s + 1) + 1;
+	}
+
+	return (n);
+}
