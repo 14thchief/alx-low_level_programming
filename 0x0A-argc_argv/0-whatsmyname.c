745e782b905bc87d5fd225b8d24b77d34e424f5a
Victor Omah <14thchief@gmail.com>
2022-10-05 18:56:17 -0700

    quick save

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * @argv: Array name
  * Return: zero
  */
-
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc * 0]);
+
 	return (0);
 }
